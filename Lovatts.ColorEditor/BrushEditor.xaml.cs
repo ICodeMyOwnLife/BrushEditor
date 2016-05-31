81a00075aa0b4411f47b3e0061945fd47debcfcb
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-31 11:01:53 +0700

    First commit

---
+++ Lovatts.ColorEditor/BrushEditor.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using BrushEditor;
+
+namespace Lovatts.ColorEditor
+{
+    /// <summary>
+    /// Interaction logic for BrushEditor.xaml
+    /// </summary>
+    public partial class BrushEditor : UserControl
+    {
+        private BrushEditorViewModel _brushEditorViewModel;
+
+        public BrushEditor()
+        {
+            InitializeComponent();
+            
+            BrushEditorViewModel = new BrushEditorViewModel();
+            BrushEditorViewModel.ColorEditorViewModel = colorEditor.ColorEditorViewModel;            
+        }
+
+        public BrushEditorViewModel BrushEditorViewModel
+        {
+            get { return _brushEditorViewModel; }
+            set { 
+                _brushEditorViewModel = value;
+
+                this.DataContext = _brushEditorViewModel;
+
+                // Should probably implement INotifyPropertyChanged
+            }
+        }
+    }
+}
